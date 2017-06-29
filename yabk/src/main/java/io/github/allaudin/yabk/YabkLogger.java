8838d593b0942ea29e43088497ad1249441fc0d4
mallaudin <mallaudinqazi@gmail.com>
2017-06-29 17:31:02 +0500

    update

--- yabk/src/main/java/io/github/allaudin/yabk/YabkLogger.java
+++ yabk/src/main/java/io/github/allaudin/yabk/YabkLogger.java
@@ -18,7 +18,7 @@ public final class YabkLogger {
         throw new AssertionError("Can't instantiate YabkLogger");
     }
 
-    public static void init(Messager messager) {
+    static void init(Messager messager) {
         instance = messager;
     }
 
@@ -26,7 +26,7 @@ public final class YabkLogger {
         instance.printMessage(Diagnostic.Kind.NOTE, String.format(format, objects));
     }
 
-    public static void error(Element e, String format, Object... objects) {
+    static void error(Element e, String format, Object... objects) {
         instance.printMessage(Diagnostic.Kind.ERROR, String.format(format, objects), e);
     }
 
