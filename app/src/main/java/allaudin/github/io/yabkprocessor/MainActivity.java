9c330e835d8e3665c2d2685602e12cd15c2b9a95
M. Allaudin <muhammad.allaudin@partner.bmwgroup.com>
2020-02-03 09:10:56 +0100

    update maven and refactor sample app

--- app/src/main/java/allaudin/github/io/yabkprocessor/MainActivity.java
+++ app/src/main/java/allaudin/github/io/yabkprocessor/MainActivity.java
@@ -1,10 +1,11 @@
 package allaudin.github.io.yabkprocessor;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-
+import allaudin.github.io.yabkprocessor.model.User;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -14,19 +15,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findViewById(R.id.send).setOnClickListener(this);
-
     }
 
     @Override
     public void onClick(View v) {
-       /* UserModel model = new UserModel();
+        User model = new User();
         model.setId(100);
         model.setName("ali");
-        model.setIsMarried(true);
-        model.setPhone("some phone");
-
         Intent intent = new Intent(this, UserActivity.class);
         intent.putExtra(UserActivity.USER, model);
-        startActivity(intent);*/
+        startActivity(intent);
     }
 }
--- app/src/main/java/allaudin/github/io/yabkprocessor/UserActivity.java
+++ app/src/main/java/allaudin/github/io/yabkprocessor/UserActivity.java
@@ -1,16 +1,29 @@
 package allaudin.github.io.yabkprocessor;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import allaudin.github.io.yabkprocessor.model.User;
 
 
 public class UserActivity extends AppCompatActivity {
 
     public static final String USER = "user";
+
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+
+        User user = getIntent().getParcelableExtra(USER);
+
+        TextView result = (TextView) findViewById(R.id.result);
+        result.setText(
+                user.getId() + " " + user.getName()
+        );
 
     }
 }
